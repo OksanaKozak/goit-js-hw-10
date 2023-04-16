926df8c61ef3cd06f154b3426e5c60757cf9ba45
OksanaKozak <oksanakozak2016@gmail.com>
2023-04-16 17:25:22 +0300

    add task

---
+++ src/fetchCountries.js
@@ -0,0 +1,12 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const url = 'https://restcountries.com/v3.1/name/';
+const filter ='fields=name,capital,population,flags,languages'
+
+export function fetchCountries(name) {
+  return fetch(`${url}${name}?${filter}`).then(response => {
+        if (!response.ok) {
+            throw Notify.failure("Oops, there is no country with that name");
+        }
+        return response.json();
+    });
+    };
